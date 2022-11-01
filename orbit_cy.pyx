219d8948744ee2b22e6d9f08e3020a59c0b3ded5
CamiloHedzz <111331587+CamiloHedzz@users.noreply.github.com>
2022-11-01 09:52:29 -0500

    Update orbit_cy.pyx

--- orbit_cy.pyx
+++ orbit_cy.pyx
@@ -1,7 +1,26 @@
+'''
+Autor: Juan Camilo Hernandez
+Tema: Cython
+
+'''
+
 from math import sqrt
 
+'''
+Se require la raiz cuadrada:
+    Se instancia como función externa
+    Se prepara para multihilo
+'''
 
-class Planet(object):
+cdef extern from "math.h":
+    double sqrt(double x) nogil
+
+
+cdef class Planet(object):
+#Variables publicas: declaración
+    
+    cdef public double x,y,z,vx,vy,vz,m
+
     def __init__(self):
     # some initial position and velocity
         self.x = 1.0
@@ -14,8 +33,11 @@ class Planet(object):
         self.m = 1.0
 
     
-def single_step_cy(planet, dt):
-        
+cdef void single_step_py(Planet planet, double dt):
+    
+    cdef float x,y,z
+    cdef double Fx, Fy, Fz, distance
+    
     # Compute force: gravity towards origin
     distance = sqrt(planet.x**2 + planet.y**2 + planet.z**2)
     Fx = -planet.x / distance**3
@@ -30,9 +52,9 @@ def single_step_cy(planet, dt):
     planet.vy += dt * Fy / planet.m
     planet.vz += dt * Fz / planet.m
 
-def step_time_cy(planet, time_span, n_steps):
+def step_time_py(planet, time_span, n_steps):
     dt = time_span / n_steps
     for j in range(n_steps):
-        single_step_cy(planet, dt)
+        single_step_py(planet, dt)
 
     
